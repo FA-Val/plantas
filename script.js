24cad990941794e4dcf1c8b1c22f3002541ec4c1
FA-Val <valfaav08@gmail.com>
2025-04-08 21:00:24 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,35 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+document.addEventListener("DOMContentLoaded", function(){
+    const imagen = document.getElementById("planta-img");
+    const texto = document.getElementById("text-descripcion");
+    imagen.addEventListener("click", function() {
+        texto.classList.toggle("mostrar");
+    });
+});
+function fijarDescripcion(elemento) {
+    const plantas = document.querySelectorAll('.planta');
+    plantas.forEach(planta => {
+        if (planta !== elemento) {
+            planta.classList.remove('fija');
+        }
+    });
+    elemento.classList.toggle('fija');
+}
+function toggleDetalle(card) {
+    const cards = document.querySelectorAll('.cos-card');
+    cards.forEach(c => {
+        if (c !== card) c.classList.remove('activa');
+    });
+
+    card.classList.toggle('activa');
+}
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll('.planta-card');
+  cards.forEach(card => {
+    card.addEventListener('click', () => {
+      card.classList.toggle('flipped');
+    });
+  });
+});
